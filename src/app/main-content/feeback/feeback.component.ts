c581c706169790898210fe23a2c0da14ec1103e2
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-06-26 15:46:20 +0200

    all main companents added

---
+++ src/app/main-content/feeback/feeback.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feeback',
+  standalone: true,
+  imports: [],
+  templateUrl: './feeback.component.html',
+  styleUrl: './feeback.component.scss'
+})
+export class FeebackComponent {
+
+}
--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { AboveToFoldComponent } from './above-to-fold/above-to-fold.component';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillSetComponent } from './skill-set/skill-set.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { FeebackComponent } from './feeback/feeback.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
   imports: [
     AboveToFoldComponent,
-    AboutMeComponent
+    AboutMeComponent,
+    SkillSetComponent,
+    ProjectsComponent,
+    FeebackComponent,
+    ContactComponent,
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
