8266151337afbcf6dfa35d2124132484e290a212
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-06-26 10:38:22 +0200

    first commit

---
+++ src/app/above-to-fold/above-to-fold.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-above-to-fold',
+  standalone: true,
+  imports: [],
+  templateUrl: './above-to-fold.component.html',
+  styleUrl: './above-to-fold.component.scss'
+})
+export class AboveToFoldComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { AboveToFoldComponent } from './above-to-fold/above-to-fold.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: AboveToFoldComponent}
+];
