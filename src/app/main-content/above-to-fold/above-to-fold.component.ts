41913f00345edb6599dd831791929cddfccda044
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-07-11 10:54:49 +0200

    dot & hover aboveFold adeed

--- src/app/main-content/above-to-fold/above-to-fold.component.ts
+++ src/app/main-content/above-to-fold/above-to-fold.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-above-to-fold',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './above-to-fold.component.html',
   styleUrl: './above-to-fold.component.scss'
 })
 export class AboveToFoldComponent {
+  isSwitched = false;
+
+  translate() {
+    this.isSwitched = !this.isSwitched;
+  }
 
 }
