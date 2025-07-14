75b224ef21c20594faac28f927103bd6e5bf3cbd
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-07-14 17:37:46 +0200

    impressum & legal notice added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 
 import { MainContentComponent } from './main-content/main-content.component';
+import { ImpressumComponent } from './impressum/impressum.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
 
 export const routes: Routes = [
-    { path: "", component: MainContentComponent}
+    { path: "", component: MainContentComponent},
+    { path: "impressum", component: ImpressumComponent },
+    { path: "legal", component: LegalNoticeComponent },
 ];
