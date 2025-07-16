b8622766baa482b45bfe9f81e6abebaad59cc2b5
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-07-16 14:35:21 +0200

    formspree added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,9 +3,11 @@ import { Routes } from '@angular/router';
 import { MainContentComponent } from './main-content/main-content.component';
 import { ImpressumComponent } from './impressum/impressum.component';
 import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { MailSendComponent } from './mail-send/mail-send.component';
 
 export const routes: Routes = [
     { path: "", component: MainContentComponent},
     { path: "impressum", component: ImpressumComponent },
     { path: "legal", component: LegalNoticeComponent },
+    { path: "send", component: MailSendComponent},
 ];
--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -3,10 +3,28 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [
+    
+  ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  sendMail(event: any){
+    event.preventDefault();
+    const data = new FormData(event.target);
+
+    fetch("https://formspree.io/f/mblkjlgr", {
+        method: "POST",
+        body: new FormData(event.target),
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(() => {
+        window.location.href = "/send";
+    }).catch((error) => {
+        console.log(error);
+    });
+}
 }
