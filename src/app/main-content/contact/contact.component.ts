39dffb61cd176d8a39dfa827702b8c05b2c595d3
NicolausFeldtmann <n.feldtmann@yahoo.de>
2025-07-21 14:27:01 +0200

    validation for email added

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,19 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { TranslationsService } from '../../translationService';
 import { LanguageService } from '../../language.service';
+import { FormsModule } from '@angular/forms';
+import { flatMap } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [
-    
+  imports: 
+  [
+    FormsModule,
+    CommonModule
   ],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent implements OnInit {
-
+    isChecked = false;
+    isActive = false;
     currenLanguage: any;
+    name: string = '';
+    email: string = '';
+    message: string= '';
 
     constructor(
         private translationService: TranslationsService,
@@ -30,20 +39,42 @@ export class ContactComponent implements OnInit {
         return this.translationService.translate(textKey, this.currenLanguage);
     }
 
-    sendMail(event: any){
-        event.preventDefault();
-        const data = new FormData(event.target);
+    check() {
+        this.isChecked = !this.isChecked;
+        this.changeBtn();
+    }
 
-        fetch("https://formspree.io/f/mblkjlgr", {
-            method: "POST",
-            body: new FormData(event.target),
-            headers: {
-                'Accept': 'application/json'
-            }
-        }).then(() => {
-            window.location.href = "/send";
-        }).catch((error) => {
-            console.log(error);
-        });
+    isFormValid(): any {
+        return(
+            this.name.trim() !== '' &&
+            this.email.trim() !== '' &&
+            this.message.trim() !== '' &&
+            this.isChecked
+        );
+    }
+
+    changeBtn() {
+        this.isActive = this.isFormValid();
+    }
+
+    sendMail(event: any){
+        if (!this.isFormValid()) {
+            return;
+        } else {
+            event.preventDefault();
+            const data = new FormData(event.target);
+
+            fetch("https://formspree.io/f/mblkjlgr", {
+                method: "POST",
+                body: new FormData(event.target),
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(() => {
+                window.location.href = "/send";
+            }).catch((error) => {
+                console.log(error);
+            });
+        }
     }
 }
